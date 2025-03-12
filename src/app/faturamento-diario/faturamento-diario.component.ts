3b26cc146fa7d936568b40924881ed2f8f599eea
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-12 20:17:29 -0300

    Criação de componente faturamento-diario

---
+++ src/app/faturamento-diario/faturamento-diario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faturamento-diario',
+  templateUrl: './faturamento-diario.component.html',
+  styleUrls: ['./faturamento-diario.component.scss']
+})
+export class FaturamentoDiarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
