d825b05bc50671677cf2ff4ebb54f3228be43bef
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-12 20:19:41 -0300

    Ajustes de rotas e modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FibonacciComponent } from './fibonacci/fibonacci.component';
 import { InitialPageComponent } from './initial-page/initial-page.component';
+import { FaturamentoDiarioComponent } from './faturamento-diario/faturamento-diario.component';
+import { FaturamentoEstadoComponent } from './faturamento-estado/faturamento-estado.component';
+import { InversaoStringComponent } from './inversao-string/inversao-string.component';
 
 const routes: Routes = [
   { path: '', component: InitialPageComponent },
   { path: 'fibonacci', component: FibonacciComponent },
+  { path: 'faturamento-diario', component: FaturamentoDiarioComponent },
+  { path: 'faturamento-estado', component: FaturamentoEstadoComponent },
+  { path: 'inversao-strings', component: InversaoStringComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FibonacciComponent } from './fibonacci/fibonacci.component';
 import { InitialPageComponent } from './initial-page/initial-page.component';
+import { FaturamentoDiarioComponent } from './faturamento-diario/faturamento-diario.component';
+import { FaturamentoEstadoComponent } from './faturamento-estado/faturamento-estado.component';
+import { InversaoStringComponent } from './inversao-string/inversao-string.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FibonacciComponent,
-    InitialPageComponent
+    InitialPageComponent,
+    FaturamentoDiarioComponent,
+    FaturamentoEstadoComponent,
+    InversaoStringComponent
   ],
   imports: [
     BrowserModule,
