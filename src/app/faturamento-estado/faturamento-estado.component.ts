5ede519f80c576f2ac21d47dfef40800fdd507fb
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-12 20:18:07 -0300

    Criação de componente faturamento-estado

---
+++ src/app/faturamento-estado/faturamento-estado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faturamento-estado',
+  templateUrl: './faturamento-estado.component.html',
+  styleUrls: ['./faturamento-estado.component.scss']
+})
+export class FaturamentoEstadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
