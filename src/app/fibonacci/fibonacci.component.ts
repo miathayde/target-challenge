c4d7ebcc5f0dac133fc5098679ecc5db7be59ae8
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-12 22:16:57 -0300

    Implementando funcionamento da tela de Fibonacci

--- src/app/fibonacci/fibonacci.component.ts
+++ src/app/fibonacci/fibonacci.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-fibonacci',
   templateUrl: './fibonacci.component.html',
   styleUrls: ['./fibonacci.component.scss']
 })
-export class FibonacciComponent implements OnInit {
+export class FibonacciComponent implements OnChanges {
+  number: number;
+  resultado: number;
+  sequenciaFibonacci: Array<number>;
+  exibirResultado: boolean = null;
 
   constructor() { }
-
-  ngOnInit(): void {
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
   }
 
+
+  gerarSequencia() {
+    this.resultado = this.number;
+    this.sequenciaFibonacci = [0, 1];
+
+    while (true) {
+      const nextFib = this.sequenciaFibonacci[this.sequenciaFibonacci.length - 1] + 
+        this.sequenciaFibonacci[this.sequenciaFibonacci.length - 2];
+      if (nextFib > this.number) break; 
+
+      this.sequenciaFibonacci.push(nextFib);
+    }
+
+    if (this.sequenciaFibonacci.includes(this.number)) {
+      this.exibirResultado = true;
+    } else {
+      this.exibirResultado = false;
+    }
+  }
+
+  limpar() {
+    this.number = null;
+    this.exibirResultado = null;
+  }
 }
