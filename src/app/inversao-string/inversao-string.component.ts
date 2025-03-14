224356a460571fcdec4bac957efab7f2c88e2954
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-13 23:14:01 -0300

    Implementação da inversão de strings

--- src/app/inversao-string/inversao-string.component.ts
+++ src/app/inversao-string/inversao-string.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inversao-string.component.scss']
 })
 export class InversaoStringComponent implements OnInit {
+  string: string;
+  stringInvertida: string;
+  exibirResultado: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  inverterString() {
+    const arrayLetras = this.string.split('');
+
+    var arrayLetrasInvertido = [];
+    arrayLetras.forEach(x => {
+      arrayLetrasInvertido.unshift(x);
+    });
+
+    this.stringInvertida = arrayLetrasInvertido.join('');
+    this.exibirResultado = true;
+  }
+
+  limpar() {
+    this.exibirResultado = false;
+    this.string = '';
+    this.stringInvertida = '';
+  }
 }
