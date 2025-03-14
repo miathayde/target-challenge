25ce4f58206bd8d9463e8dd42480e69619bc837e
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-13 22:30:58 -0300

    Implementação de faturamento por estado

--- src/app/faturamento-estado/faturamento-estado.component.ts
+++ src/app/faturamento-estado/faturamento-estado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FaturamentoEstado } from '../models/faturamento-estado';
 
 @Component({
   selector: 'app-faturamento-estado',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faturamento-estado.component.scss']
 })
 export class FaturamentoEstadoComponent implements OnInit {
+  faturamentosMensaisEstados: Array<FaturamentoEstado> = [
+    {estado: 'SP', faturamento: 67836.43, percentualFaturamento: 0},
+    {estado: 'RJ', faturamento: 36678.66, percentualFaturamento: 0},
+    {estado: 'MG', faturamento: 29229.88, percentualFaturamento: 0},
+    {estado: 'ES', faturamento: 27165.48, percentualFaturamento: 0},
+    {estado: 'Outros', faturamento: 19849.53, percentualFaturamento: 0}
+  ];
+  totalFaturamentos: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.calcularPercentualFaturamento();
   }
 
+  calcularPercentualFaturamento() {
+    this.totalFaturamentos = this.obterTotalFaturamentos();
+
+    this.faturamentosMensaisEstados.forEach(x => {
+      x.percentualFaturamento = Number(((x.faturamento / this.totalFaturamentos) * 100).toFixed(2));
+    });
+  }
+
+  obterTotalFaturamentos(): number {
+    var total = 0;
+    this.faturamentosMensaisEstados.forEach(x => {
+      total += x.faturamento;
+    });
+
+    return total;
+  }
 }
